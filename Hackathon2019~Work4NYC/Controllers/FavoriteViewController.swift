2dde2ea58d39e94a9caa63f4d7b33461c9ac218a
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-03-27 16:25:39 -0400

    creating the table view

--- Hackathon2019~Work4NYC/Controllers/FavoriteViewController.swift
+++ Hackathon2019~Work4NYC/Controllers/FavoriteViewController.swift
@@ -9,22 +9,19 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
-
+let controlView = FavoriteView()
+    var jobs = [Job]() {
+        didSet{
+            self.controlView.favoriteView.reloadData()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(controlView)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
