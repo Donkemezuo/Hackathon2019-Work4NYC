3dd85bd03f423e04e7bc5e90659f25a28076ec6d
Donkemezuo <raymonddonkemezuo.org>
2019-03-27 12:09:21 -0400

    first commit. Main files created

---
+++ Hackathon2019~Work4NYC/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackathon2019~Work4NYC
+//
+//  Created by Donkemezuo Raymond Tariladou on 3/27/19.
+//  Copyright © 2019 EnProTech Group. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
