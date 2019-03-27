fe842e45f32597f7192e70db674faa22aef7de77
BironSu <bironsu@pursuit.org>
2019-03-27 18:08:42 -0400

    finished Detail, need data to populate. Finished WebView

--- Hackathon2019~Work4NYC/Controllers/DetailViewController.swift
+++ Hackathon2019~Work4NYC/Controllers/DetailViewController.swift
@@ -10,21 +10,43 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var job: Job?
+    let detailView = DetailView()
+    let webViewController = WebViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(detailView)
+        detailView.nycGovButton.addTarget(self, action: #selector(nycGovSegue), for: .touchUpInside)
+        detailView.monsterButton.addTarget(self, action: #selector(monsterSegue), for: .touchUpInside)
+        detailView.indeedButton.addTarget(self, action: #selector(indeedSegue), for: .touchUpInside)
+        detailView.linkedInButton.addTarget(self, action: #selector(linkedInSegue), for: .touchUpInside)
     }
-    
+    @objc func nycGovSegue() {
+        let keyword = job?.business_title.replacingOccurrences(of: " ", with: "")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let urlLink = "https://a127-jobs.nyc.gov/index_new.html?keyword=\(keyword!)"
+        webViewController.urlLink = urlLink
+        self.navigationController?.pushViewController(webViewController, animated: true)
     }
-    */
+    @objc func monsterSegue() {
+        let keyword = job?.business_title.replacingOccurrences(of: " ", with: "")
 
+        let urlLink = "https://www.monster.com/jobs/search?q=\(keyword!)"
+        webViewController.urlLink = urlLink
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
+    @objc func indeedSegue() {
+        let keyword = job?.business_title.replacingOccurrences(of: " ", with: "")
+
+        let urlLink = "https://www.indeed.com/jobs?q=\(keyword!)&l="
+        webViewController.urlLink = urlLink
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
+    @objc func linkedInSegue() {
+        let keyword = job?.business_title.replacingOccurrences(of: " ", with: "")
+
+        let urlLink = "https://www.linkedin.com/jobs/search?keywords=\(keyword!)&pageNum=0&position=1"
+        webViewController.urlLink = urlLink
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
 }
