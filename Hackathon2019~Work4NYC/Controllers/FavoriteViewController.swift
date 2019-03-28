3589caa0fcc5b27f08c2be4381c965dce4e33d7e
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-03-28 11:24:36 -0400

    working on save

--- Hackathon2019~Work4NYC/Controllers/FavoriteViewController.swift
+++ Hackathon2019~Work4NYC/Controllers/FavoriteViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
 let controlView = FavoriteView()
+//    var jobs = [Job]()
     var jobs = [Job]() {
         didSet{
-            self.controlView.favoriteView.reloadData()
+            DispatchQueue.main.async {
+        self.controlView.favoriteView.reloadData()
+            }
         }
     }
     override func viewDidLoad() {
@@ -21,12 +24,22 @@ let controlView = FavoriteView()
         view.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.2261782289, alpha: 1)
         controlView.favoriteView.delegate = self
         controlView.favoriteView.dataSource = self
+        populateData(keyword: "")
+    }
+    func populateData(keyword: String) {
+        JobAPIClient.getJobs(keyword: keyword) { (error, data) in
+            if let error = error {
+                print("Error getting data : \(error)")
+            } else if let data = data {
+                self.jobs = data
+            }
+        }
     }
 }
 
 extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return jobs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,12 +49,18 @@ extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
         favoriteCell.titleLabel.text = "Job Title"
         favoriteCell.backgroundColor = .clear
         favoriteCell.layer.borderWidth = 2
-        favoriteCell.layer.cornerRadius = 1
+        favoriteCell.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        favoriteCell.layer.cornerRadius = 5
         return favoriteCell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
-    
+    func tableView(_tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.job = self.jobs[indexPath.row]
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
